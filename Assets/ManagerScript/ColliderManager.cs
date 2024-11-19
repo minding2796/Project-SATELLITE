6809b0ebbde777254f23d581d8273d042c908347
minding2796 <minding2796@gmail.com>
2024-11-19 17:27:35 +0900

    Add some features
    convert map to tilemap
    add accelerator
    add jump wall
    
    Signed-off-by: minding2796 <minding2796@gmail.com>

--- Assets/ManagerScript/ColliderManager.cs
+++ Assets/ManagerScript/ColliderManager.cs
@@ -6,9 +6,12 @@ namespace ManagerScript
     public class ColliderManager : MonoBehaviour
     {
         public Button.ButtonClickedEvent onTriggerEnter2D;
+        public Button.ButtonClickedEvent onceTriggerEnter2D;
 
         public void OnTriggerEnter2D(Collider2D other)
         {
+            onceTriggerEnter2D?.Invoke();
+            onceTriggerEnter2D = null;
             onTriggerEnter2D.Invoke();
         }
     }
--- Assets/UIScript/TextScriptProcessor.cs
+++ Assets/UIScript/TextScriptProcessor.cs
@@ -11,11 +11,21 @@ namespace UIScript
         [SerializeField] private Button.ButtonClickedEvent onScriptFinished;
         private TextMeshProUGUI _text;
         private string[] _scripts;
+        private bool[] _importance;
         private int _index;
         private void Start()
         {
             _text = GetComponent<TextMeshProUGUI>();
-            _scripts = Resources.Load<TextAsset>(scriptPath).text.Split("∮").Select(s => s.Trim()).ToArray();
+            LoadScript(scriptPath);
+        }
+
+        public void Skip()
+        {
+            do Next(); while (_index < _importance.Length && !_importance[_index]);
+        }
+        
+        public void ResetScript()
+        {
             _text.text = _scripts[_index = 0];
         }
 
@@ -23,6 +33,24 @@ namespace UIScript
         {
             if (++_index < _scripts.Length) _text.text = _scripts[_index];
             else onScriptFinished.Invoke();
+        }
+
+        public void Previous()
+        {
+            if (--_index >= 0) _text.text = _scripts[_index];
+        }
+
+        public void LoadScript(string path)
+        {
+            _scripts = Resources.Load<TextAsset>(path).text.Split("∮").Select(s => s.Trim()).ToArray();
+            _importance = new bool[_scripts.Length];
+            for (var i = 0; i < _scripts.Length; i++)
+            {
+                if (!_scripts[i].StartsWith("!important")) continue;
+                _importance[i] = true;
+                _scripts[i] = _scripts[i]["!important".Length..];
+            }
+            ResetScript();
         }
     }
 }

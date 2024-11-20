78051808c54655b7923d122fd8855b7a9987208f
minding2796 <minding2796@gmail.com>
2024-11-20 20:29:25 +0900

    update version
    Prevent Player Void-Jumping
    Change Map
    
    Signed-off-by: minding2796 <minding2796@gmail.com>

--- Assets/PlayerScript/PlayerMove.cs
+++ Assets/PlayerScript/PlayerMove.cs
@@ -28,6 +28,7 @@ namespace PlayerScript
             transform.rotation = Quaternion.Euler(EulerAngles);
             _rigidbody2D.velocity = new Vector2(MoveButtons.Direction * speed * (_rigidbody2D.IsTouchingLayers(LayerMask.GetMask("Accelerator")) ? 2f : 1f), MoveButtons.TriggerJump ? jumpForce * (IsGravityReversed ? -1 : 1) : _rigidbody2D.velocity.y);
             if (!MoveButtons.TriggerJump && (IsGravityReversed ? _rigidbody2D.velocity.y > jumpForce * -0.75f : _rigidbody2D.velocity.y < jumpForce * 0.75f) && _rigidbody2D.IsTouchingLayers(LayerMask.GetMask("Ground"))) JumpCount = maxJumpCount;
+            else if (JumpCount == maxJumpCount) JumpCount = maxJumpCount - 1;
             MoveButtons.TriggerJump = false;
         }
     }
